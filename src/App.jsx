d0e876053288e90cba4508b70e5809fbd78f8ffd
navnihalsatpute <128957876+navnihalsatpute@users.noreply.github.com>
2025-10-22 18:40:45 +0530

    Initial commit of a new clean React App

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-center pt-8">Contacts App</h1>
+    </div>
+  );
+}
+
+export default App;
